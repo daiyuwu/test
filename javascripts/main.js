d3349f06f3aba75c1d5e9050bc01da2d89390356
daiyuwu <taiyue.tw@gmail.com>
2016-11-06 21:13:10 +0800

    br

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,26 @@
-console.log('This would be the main JS file.');
+console.log('-- PREPROCESS --');
+var $printTarget = document.getElementById("test_print_block");
+
+console.log('-- START --');
+var times3 = 3, times5 = 5, timesboth = times3*times5;
+
+print('test');
+print('test2');
+
+function print(msg) {
+	appendPrint(msg);
+}
+
+function normalPrint(msg) {
+	$printTarget.innerText = msg;
+}
+
+function appendPrint(msg) {
+	var tmpContent = $printTarget.innerText;
+	tmpContent += msg + "\r";
+	$printTarget.innerText = tmpContent;
+}
+
+// function AIPrint(msg) {
+	// $printTarget.innerText = msg + "<span class='new_line'></span>";
+// }
